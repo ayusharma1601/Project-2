ad8e8516f46ced1f29ef088482da108a58dabb64
ayusharma1601 <150792017+ayusharma1601@users.noreply.github.com>
2025-08-31 15:34:10 +0530

    library.py

--- library.py
+++ library.py
@@ -1,9 +1,9 @@
 dic={"Utopia":"Thomas More",
      "Odyssey":"Homer",
      "Othello":"William Shakespeare"}
+
 while True:
     print("Welcome to the Library Management System")
-    print("Here are the books we have:")
     
     options=input("Enter what do you want to do?\n1. Check what books we have\n2. Add books of your choice\n3. search a particular book\n4 exit ")
 
@@ -20,10 +20,13 @@ while True:
             more = input("Add another book? (y/n): ").lower()
             if more != "y":
                 break
+
+     
     def show(item_view):
         for key ,value in item_view.items():
             print(f"{key}:{value}")
 
+     
     def search():
         find=input("what book you wanna search?: ").title()
         if find in dic:
@@ -32,14 +35,19 @@ while True:
             print(f"{find} by {dic[find]} found")
         else:
             print("Book not found")
+             
     if options=="1":
         show(dic)
+         
     elif options=="2":
         add()
+         
     elif options=="3":
         search()
+         
     elif options=="4":
         print("Thank you for using the Library Management System")
         break
+         
     else:
         print("Invalid Input")
